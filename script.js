6f4835025833457f6fb86008d47e0a320a475b55
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-05-26 14:43:10 -0500

    creating random characters

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
 // Assignment code here
+var length = Number(prompt("Choose number of characters for password"));
+
+var upChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var loChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var numChar = ["1","2","3","4","5","6","7","8","9","0"];
+
+var spChar = ["!","@","#","$","%","^","&","*","(",")","<",">","+","=","?","/"];
+
+var charSet = [upChar,loChar,numChar,spChar];
+
+var Type = (prompt("Choose Character Type:Uppercase, lowercase, number, special"));
+  
+  if (Type==="uppercase" || Type==="UPPERCASE");
+ 
+  var randomNumber= function() {
+
+    var value= Math.floor(Math.random()*(26))+2;
+
+    return value;
+  
+  }
+
+  ();
+
+  console.log(upChar[randomNumber]);
+
+
+  
+  if (Type==="lowercase" || Type==="LOWERCASE");
+
+  var randomNumber= function() {
+
+    var value= Math.floor(Math.random()*(26))+2;
+
+    return value;
+  
+  }
+
+  ();
+
+  console.log(loChar[randomNumber]);
+
+
+  if (Type==="numerical" || Type==="NUMERICAL");
+
+  var randomNumber= function() {
+
+    var value= Math.floor(Math.random()*(9));
+
+    return value;
+  
+  }
+
+  ();
+
+  console.log(numChar[randomNumber]);
+
+
+
+  if (Type==="special" || Type==="SPECIAL");
+
+
+  var randomNumber= function() {
+
+    var value= Math.floor(Math.random()*(5))+2;
+
+    return value;
+  
+  }
+
+  ();
+
+  console.log(spChar[randomNumber]);
+
+
+
+
+
+
+  //}
+//}
+
+
 
 
 // Get references to the #generate element
