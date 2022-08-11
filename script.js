8b3bd1f3318056c4e1c264c5df4694756e8482b8
asaelens02 <104400626+asaelens02@users.noreply.github.com>
2022-08-10 20:16:29 -0500

    uploaded wrong js previously

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 
 
-var length = Number(prompt("Choose number of characters for password"));
+
 
 var upChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
@@ -11,70 +11,147 @@ var numChar = ["1","2","3","4","5","6","7","8","9","0"];
 
 var spChar = ["!","@","#","$","%","^","&","*","(",")","<",">","+","=","?","/"];
 
-var charSet = [upChar,loChar,numChar,spChar];
+//var charSet = [upChar,loChar,numChar,spChar];
 
-var Type = (prompt("Choose Character Type:Uppercase, lowercase, number, special"));
+function generatePassword() {
+
+  var password= "";
+  var pChar="";
+
+  var length= Number(prompt("How many characters would you like your password to be? Choose a number between 8 and 128"));
+
+  length=parseInt(length);
+
+  alert("Please follow the prompts to select character categories for your password");
+
+
+
+  var loCharSelect = confirm("Would you like to use lowercase characters?");
+
+  if (loCharSelect) {
+
+   var randomNumber= function() {
+
+   var value= Math.floor(Math.random()*(26))+2;
   
-if (Type==="uppercase" || Type==="UPPERCASE") 
+    return value;
+
+   }
+
+    pChar += loChar[randomNumber];
+
+    
+  }
+
+  var upCharSelect = confirm ("Would you like to use UPPERCASE characters?");
+
+  if (upCharSelect){
+
+    pChar += upChar;
+  }
+
+  var numCharSelect= confirm ("Would you like to use numbers in your password?");
+
+  if (numCharSelect){
+
+    pChar += numChar;
+
+  }
+
+  var spCharSelect = confirm ("Lastly, would you like to make your password extra secure using special characters?");
+
+  if (spCharSelect){
+
+    pChar += spChar;
+  }
+
+  for(var i=0; i<length; i++){
+
+    password=pChar[Math.floor(Math.random()*pChar.length)];
+
+  }
+
+}
+
+//generatePassword();
+
+var generateBtn= document.querySelector("#generate");
+  
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+generateBtn.addEventListener("click", writePassword)
+
+//writePassword();
+//var Type = (prompt("Choose Character Type:Uppercase, lowercase, number, special"));
+  
+//if (Type==="uppercase" || Type==="UPPERCASE") 
  
-  var randomNumber=  function randomNumber() {
+  //var randomNumber=  function randomNumber() {
 
-   var value = Math.floor(Math.random() * (26)) + 2;
+   //var value = Math.floor(Math.random() * (26)) + 2;
 
-    return value;
+    //return value;
   
-  }
+  //}
 
-()
+///()
   
-    console.log(upChar[randomNumber]);
+   // console.log(upChar[randomNumber]);
   
 
-if  (Type==="lowercase" || Type==="LOWERCASE") 
+//if  (Type==="lowercase" || Type==="LOWERCASE") 
 
-  var randomNumber= function() {
+  // var randomNumber= function() {
 
-    var value= Math.floor(Math.random()*(26))+2;
+  //   var value= Math.floor(Math.random()*(26))+2;
   
-    return value;
+  //   return value;
 
-  }
+  // }
 
-  ()
+  // ()
 
-  console.log (loChar[randomNumber]);
+  // console.log (loChar[randomNumber]);
 
 
-  if (Type==="numerical" || Type==="NUMERICAL");
+  // if (Type==="numerical" || Type==="NUMERICAL");
 
-  var randomNumber= function() {
+  // var randomNumber= function() {
 
-    var value= Math.floor(Math.random()*(9));
+  //   var value= Math.floor(Math.random()*(9));
 
-    return value;
+  //   return value;
   
-  }
+  // }
 
-  ()
+  // ()
 
-  console.log(numChar[randomNumber]);
-
-
-
-  if (Type==="special" || Type==="SPECIAL");
+  // console.log(numChar[randomNumber]);
 
 
-  var randomNumber= function() {
 
-    var value= Math.floor(Math.random()*(5))+2;
+  // if (Type==="special" || Type==="SPECIAL");
 
-    return value;
+
+  // var randomNumber= function() {
+
+  //   var value= Math.floor(Math.random()*(5))+2;
+
+  //   return value;
   
-  }
+  // }
 
-  ()
+  // ()
 
-  console.log(spChar[randomNumber])
+
+
+  //console.log(spChar[randomNumber])
 
 
 
@@ -89,16 +166,10 @@ if  (Type==="lowercase" || Type==="LOWERCASE")
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+//var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
